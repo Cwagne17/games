daccf3085adb49894fe6426cc06106af4b41b97c
Christopher Wagner <christopher.wagner@secured.team>
2024-09-01 17:23:34 -0400

    add basic app load

--- champions-conquest/main.py
+++ champions-conquest/main.py
@@ -1,7 +1,43 @@
+import pygame
+from pygame.locals import *
+ 
+class App:
+    def __init__(self):
+        self._running = True
+        self._display_surf = None
+        self.size = self.width, self.height = 1280, 720
 
-def main():
-    pass
+        # Init the game the display
+        pygame.init()
+        self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
+        self._running = True
+ 
+    def on_event(self, event):
+        """"Handles native and custom pygame events that are triggered by the user"""
+        if event.type == pygame.QUIT:
+            self._running = False
 
-
-if __name__ == "__main__":
-    main()
+    def on_loop(self):
+        """"Hanldes the game play including key presses, mouse clicks, and game specifc logic like moving the player"""
+        pass
+    
+    def on_render(self):
+        """"
+        """
+        pass
+    
+    def on_cleanup(self):
+        pygame.quit()
+ 
+    def on_execute(self):
+        """Main game loop that runs the game"""
+        while( self._running ):
+            for event in pygame.event.get():
+                self.on_event(event)
+            self.on_loop()
+            self.on_render()
+        self.on_cleanup()
+ 
+if __name__ == "__main__" :
+    ccApp = App()
+    ccApp.on_execute()
