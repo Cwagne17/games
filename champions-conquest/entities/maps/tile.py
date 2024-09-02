a50fdefd0ab67c143f8e9759b6f5e0655ff13b67
Christopher Wagner <christopher.wagner@secured.team>
2024-09-01 21:55:37 -0400

    save default map

--- champions-conquest/entities/maps/tile.py
+++ champions-conquest/entities/maps/tile.py
@@ -1,9 +1,21 @@
-from enum import Enum
-
 ASSETS_PATH = "assets/Tiles/"
 
-class Tile(Enum):
-    # Grass tile types
-    GRASS = ASSETS_PATH + "grass/grass.png"
-    
-    WATER = ASSETS_PATH + "water/water.png"
+# Tile Constants
+# The naming convention being used for the different tile types is as follows:
+# [Tile Type]_[Tile Variant]
+#
+# For example, each tile has many types based on the edges, corners, and center of the tile.
+#
+
+# Grass tile types
+G_c = ASSETS_PATH + "grass/grass_c.png"
+G_tlc = ASSETS_PATH + "grass/grass_tlc.png"
+G_le = ASSETS_PATH + "grass/grass_le.png"
+G_blc = ASSETS_PATH + "grass/grass_blc.png"
+G_be = ASSETS_PATH + "grass/grass_be.png"
+G_brc = ASSETS_PATH + "grass/grass_brc.png"
+G_re = ASSETS_PATH + "grass/grass_re.png"
+G_trc = ASSETS_PATH + "grass/grass_trc.png"
+G_te = ASSETS_PATH + "grass/grass_te.png"
+
+W_c = ASSETS_PATH + "water/water.png"
