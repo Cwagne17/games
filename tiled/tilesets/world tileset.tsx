524a81393d59fdcc9773edbfdf03e0a5e264b835
Christopher Wagner <christopher.wagner@secured.team>
2024-09-03 16:47:04 -0400

    add everything else

---
+++ tiled/tilesets/world tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="world tileset" tilewidth="32" tileheight="32" tilecount="840" columns="20">
+ <image source="../graphics/tilesets/world_tileset.png" width="640" height="1344"/>
+</tileset>
